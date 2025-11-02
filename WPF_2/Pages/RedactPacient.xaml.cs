17a35ad5fbc15e91248710e36d85431fa568a00c
volodya52 <pluhinvladimir54@gmail.com>
2025-11-03 00:55:08 +0500

    added validators

--- WPF_2/Pages/RedactPacient.xaml.cs
+++ WPF_2/Pages/RedactPacient.xaml.cs
@@ -29,25 +29,15 @@ namespace WPF_2.Pages
     public partial class RedactPacient : Page
     {
         private ObservableCollection<Pacient> _pacients;
-        private Pacient originalpacient;
-        public Pacient CurrentPacient { get;set; }
+        public Pacient CurrentPacient { get; set; }
+       
         public RedactPacient(ObservableCollection<Pacient> pacients, Pacient pacient)
         {
             InitializeComponent();
+            _pacients = pacients;
+            CurrentPacient = pacient;
             DataContext = this;
-            _pacients= pacients;
-            originalpacient=pacient;
-            CurrentPacient = new Pacient
-            {
-                PacientId=pacient.PacientId,
-                PacientName=pacient.PacientName,
-                PacientSurname=pacient.PacientSurname,
-                PacientLastName=pacient.PacientLastName,
-                PacientBirthday=pacient.PacientBirthday,
-                PacientPhoneNumber=pacient.PacientPhoneNumber,
-                Appoitments = pacient.Appoitments,
-            };
-            
+
 
         }
 
@@ -58,20 +48,17 @@ namespace WPF_2.Pages
 
         private void SaveButton(object sender, RoutedEventArgs e)
         {
-            originalpacient.PacientName = CurrentPacient.PacientName;  
-            originalpacient.PacientSurname = CurrentPacient.PacientSurname;
-            originalpacient.PacientLastName= CurrentPacient.PacientLastName;
-            originalpacient.PacientBirthday=CurrentPacient.PacientBirthday;
-            originalpacient.PacientPhoneNumber=CurrentPacient.PacientPhoneNumber;
+           
+
             var options = new JsonSerializerOptions
             {
                 Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
                 WriteIndented = true
             };
-            string json = JsonSerializer.Serialize(originalpacient, options);
+            string json = JsonSerializer.Serialize(CurrentPacient, options);
 
-            File.WriteAllText($"P_{originalpacient.PacientId}.json", json);
-            MessageBox.Show($"Данные пациента {originalpacient.PacientId} обновлены");
+            File.WriteAllText($"P_{CurrentPacient.PacientId}.json", json);
+            MessageBox.Show($"Данные пациента {CurrentPacient.PacientId} обновлены");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
---
+++ WPF_2/Validators/RequiredRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_2.Validators
+{
+    public class RequiredRule: ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var input = (value ?? "").ToString().Trim();
+            if (input == string.Empty)
+            {
+                return new ValidationResult(false, "Поле необходимо заполнить");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
